3f18b6e2f047c5c9741fda6336388414c93e3025
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2015-09-11 11:58:15 -0400

    test HtmlUnit

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -2,12 +2,20 @@ package myapp;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
 
 public class DemoServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println("{ \"name\": \"World\" }");
+        //resp.getWriter().println("{ \"name\": \"World\"");
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("http://catalog.cincinnatilibrary.org");
+            final String pageAsXml = page.asXml();
+            resp.getWriter().println(pageAsXml);
+        }
+        //resp.getWriter().println" }");
     }
 }
